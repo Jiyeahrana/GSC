a9ffb872ef3319997df939bf6b4a69d3b22cb874
Saurabh Sati <saurabhsati@Saurabhs-MacBook-Air.local>
2026-06-09 02:44:20 +0530

    Add frontend Firebase config

---
+++ public/config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAMVcpEGH1N8bHi7BFVabo_Y8jHHJmg0ps",
+  authDomain: "port-management-gsc.firebaseapp.com",
+  databaseURL: "https://port-management-gsc-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "port-management-gsc",
+  storageBucket: "port-management-gsc.firebasestorage.app",
+  messagingSenderId: "559633899238",
+  appId: "1:559633899238:web:38a6218201621136812527"
+};
